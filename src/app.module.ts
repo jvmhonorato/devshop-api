856110ba79bfff3cb278ba2406a7c7c2678ee5d5
vituhonorato <vituhonorato.89@gmail.com>
2022-11-09 17:41:24 -0300

    add TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,20 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { CategoryModule } from './category/category.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import  {TypeOrmModule} from '@nestjs/typeorm'
+import { Category } from './category/category.entity';
 
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      url:'postgres://postgres:mustafar2013@localhost:5432/devshop',
+      autoLoadEntities:true,
+      synchronize:true,
+      entities: [Category],
+      logging:true
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
