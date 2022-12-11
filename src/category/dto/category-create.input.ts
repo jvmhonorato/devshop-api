60bd98876b0c6ed507f3264858811c3a7c9e922e
vituhonorato <vituhonorato.89@gmail.com>
2022-12-11 14:59:17 -0300

    validate slug with RegEx

--- src/category/dto/category-create.input.ts
+++ src/category/dto/category-create.input.ts
@@ -1,5 +1,5 @@
 import { Field, InputType } from "@nestjs/graphql";
-import { Length, Validate } from 'class-validator'
+import { Length, Matches, Validate } from 'class-validator'
 import { CategorySlugIsUnique } from "../validations/CategorySlugIsUnique";
 
 @InputType()
@@ -10,6 +10,7 @@ export class CategoryCreateInput{
 
     @Field()
     @Length(4,20)
+    @Matches(/^[a-z0-9+(?:-[a-z0-9]+)*$/)
     @Validate(CategorySlugIsUnique)
     slug: string
 }
