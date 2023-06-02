52ac6763c16fb60793f805e60002fafeb2109308
vituhonorato <vituhonorato.89@gmail.com>
2023-06-02 14:58:16 -0300

    fix eslinttrc 2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { ValidationPipe } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core';
-import { useContainer } from 'class-validator';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { useContainer } from 'class-validator'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  useContainer(app.select(AppModule),{fallbackOnErrors: true})
+  const app = await NestFactory.create(AppModule)
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
