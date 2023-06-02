21978b35646e51f1432478065e0845fd8ba08f38
vituhonorato <vituhonorato.89@gmail.com>
2023-06-02 15:11:29 -0300

    fix eslinttrc in product.resolver

--- src/product/product.resolver.ts
+++ src/product/product.resolver.ts
@@ -1,63 +1,61 @@
+/* eslint-disable prettier/prettier */
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-
-import { ProductPublic } from "./dto/product";
-import { ProductCreateInput } from "./dto/product-create.input";
-import { ProductUpdateInput } from "./dto/product-update.input";
-import { Product } from "./product.entity";
-import { ProductMapper } from "./product.mapper";
-import { ProductService } from "./product.service";
+import { ProductPublic } from './dto/product'
+import { ProductCreateInput } from './dto/product-create.input'
+import { ProductUpdateInput } from './dto/product-update.input'
+import { Product } from './product.entity'
+import { ProductMapper } from './product.mapper'
+import { ProductService } from './product.service'
 
 @Resolver(of => ProductPublic)
 export class ProductResolver {
-   constructor(private readonly productService: ProductService){}
+  constructor(private readonly productService: ProductService) {}
 
-   @Query(returns => [ProductPublic], {name: 'getAllProducts'})
-   async getAllProducts(): Promise<ProductPublic[]>{
+  @Query(returns => [ProductPublic], { name: 'getAllProducts' })
+  async getAllProducts(): Promise<ProductPublic[]> {
     const products = await this.productService.findAll()
-    return products.map(ProductMapper.fromEntityToPublic)  
-   }
+    return products.map(ProductMapper.fromEntityToPublic)
+  }
 
-    //Query read by id
-    @Query(returns => ProductPublic, { name: 'getProductById'})
-    async getProductById(
-        @Args('id') id: string
-        ): Promise<ProductPublic> {
-        return  ProductMapper.fromEntityToPublic(await this.productService.findById(id)) 
-    }
+  //Query read by id
+  @Query(returns => ProductPublic, { name: 'getProductById' })
+  async getProductById(@Args('id') id: string): Promise<ProductPublic> {
+    return ProductMapper.fromEntityToPublic(
+      await this.productService.findById(id)
+    )
+  }
 
-    //Query read by slug
-    @Query(returns => ProductPublic, { name: 'getProductBySlug'})
-    async getProductBySlug(
-        @Args('slug') slug: string
-        ): Promise<ProductPublic> {
-            return  ProductMapper.fromEntityToPublic( await this.productService.findBySlug(slug))
-    }
+  //Query read by slug
+  @Query(returns => ProductPublic, { name: 'getProductBySlug' })
+  async getProductBySlug(@Args('slug') slug: string): Promise<ProductPublic> {
+    return ProductMapper.fromEntityToPublic(
+      await this.productService.findBySlug(slug)
+    )
+  }
 
-     //Mutation  create product input
-     @Mutation(returns => ProductPublic, {name: 'createProduct' })
-     async createProduct(
-        @Args('input') input: ProductCreateInput
-     ): Promise<ProductPublic> {
-           
-         return ProductMapper.fromEntityToPublic( await this.productService.create(ProductMapper.toEntity(input))) 
-     }
+  //Mutation  create product input
+  @Mutation(returns => ProductPublic, { name: 'createProduct' })
+  async createProduct(
+    @Args('input') input: ProductCreateInput
+  ): Promise<ProductPublic> {
+    return ProductMapper.fromEntityToPublic(
+      await this.productService.create(ProductMapper.toEntity(input))
+    )
+  }
 
-     @Mutation(returns => ProductPublic, {name: 'updateProduct' })
-     async updateProduct(
-        @Args('input') input: ProductUpdateInput
-     ): Promise<ProductPublic> {
-           
-         return ProductMapper.fromEntityToPublic(await this.productService.update(ProductMapper.fromUpdateToEntity(input))) 
-     }
-     
-      //Mutation  delete product input
-      @Mutation(returns => Boolean, {name: 'deleteProduct' })
-      async deleteProduct(
-         @Args('id') input: string
-      ): Promise<boolean> {
-          
-          return this.productService.delete(input)
-      }
+  @Mutation(returns => ProductPublic, { name: 'updateProduct' })
+  async updateProduct(
+    @Args('input') input: ProductUpdateInput
+  ): Promise<ProductPublic> {
+    return ProductMapper.fromEntityToPublic(
+      await this.productService.update(ProductMapper.fromUpdateToEntity(input))
+    )
+  }
 
+  //Mutation  delete product input
+  @Mutation(returns => Boolean, { name: 'deleteProduct' })
+  async deleteProduct(@Args('id') input: string): Promise<boolean> {
+    return this.productService.delete(input)
+  }
 }
